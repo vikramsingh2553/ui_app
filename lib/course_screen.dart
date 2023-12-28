88f3cf7cd09ea1e85611871027f07848a238bc4c
vikramsingh2553 <vc905371@gmail.com>
2023-12-28 10:33:45 +0530

    botoom bar navigation add

--- lib/course_screen.dart
+++ lib/course_screen.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ui_app/bottom_navigatorbar.dart';
 
 class CourseScreen extends StatelessWidget {
   const CourseScreen({super.key});
@@ -13,6 +14,13 @@ class CourseScreen extends StatelessWidget {
       'Positive Rotation',
       'Fun Practice',
       'Wrong title set',
+
+
+    ];
+    List courseText =[
+      "3 Goals Today ",
+      'three by four',
+      '2019-10-3',
 
     ];
     List<Icon>courseIconsData=[
@@ -30,213 +38,220 @@ class CourseScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: Padding(padding: const EdgeInsets.only(top: 15,right: 15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Course Page',
-                    style: txtThem.titleLarge,
-                  ),
-                  const Icon(Icons.post_add,
-                    color: Color(0xFF3CB792),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 5,),
-              const Row(
-                children: [
-                  Text("Today's math profress\t",
-                    style: TextStyle(color: Colors.black45),
-                  ),
-                  Text("0 min",
-                    style: TextStyle(color: Color(0xFF3CB792)),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 8,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text('Mathematics',style: txtThem.titleLarge,),
-                  const Text('Engilsh',style: txtStyle,),
-                  const Text('Physics',style: txtStyle),
-                  const Text('Chemistry',style: txtStyle),
+        body: Padding(padding: const EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Course Page',
+                      style: txtThem.titleLarge,
+                    ),
+                    const Icon(Icons.post_add,
+                      color: Color(0xFF3CB792),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 5,),
+                const Row(
+                  children: [
+                    Text("Today's math profress\t",
+                      style: TextStyle(color: Colors.black45),
+                    ),
+                    Text("0 min",
+                      style: TextStyle(color: Color(0xFF3CB792)),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('Mathematics',style: txtThem.titleLarge,),
+                    const Text('English',style: txtStyle,),
+                    const Text('Physics',style: txtStyle),
+                    const Text('Chemistry',style: txtStyle),
 
-                ],
-              ),
-              const SizedBox(height: 20,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(24),
-                    height: 150,
-                    width: 120,
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF5AD995),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          height: 40,
-                          width: 40,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: const Center(
-                            child: Icon(
-                              Icons.bookmark,
-                              color: Color(0xFF5AD995),
-                            ),
-                          ),
-                        ),
-                        const Text('Test',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                          ),
-                        ),
-                        const Text('Chapter Knowoledge',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 13,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(22),
-                    height: 150,
-                    width: 120,
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF5EBBFD),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          height: 40,
-                          width: 40,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: const Center(
-                            child: Icon(
-                              Icons.font_download_off_outlined,
-                              color: Color(0xFF5EBBFD),
-                            ),
-                          ),
-                        ),
-                        const Text('Summarize',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                          ),
-                        ),
-                        const Text('Study notes',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 13,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 30,),
-              Text('Recommended Course',
-                style: txtThem.titleLarge,
-              ),
-              const SizedBox(height: 5,),
-              const Text('I learned Chapter 6 last time',
-                style: txtStyle,
-              ),
-              const SizedBox(height: 20,),
-              for(var i=0; i<3; i++)
-                Container(
-                  margin: const EdgeInsets.symmetric(vertical: 10),
-                  height: 50,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(5),
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Colors.black12,blurRadius: 5,
-                        ),
-
-
-                      ]
-                  ),
-                  child:Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        height: 40,
-                        width: 40,
-                        decoration: BoxDecoration(
-                          color: bgColors[i],
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Center(
-                          child: courseIconsData[i],
-                        ),
+                  ],
+                ),
+                const SizedBox(height: 20,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(24),
+                      height: 170,
+                      width: 140,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF5AD995),
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                      Column(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
                         crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text(
-                            courseTitle[i],
-                            style: txtThem.titleMedium,
-
+                          Container(
+                            height: 40,
+                            width: 40,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: const Center(
+                              child: Icon(
+                                Icons.bookmark,
+                                color: Color(0xFF5AD995),
+                              ),
+                            ),
                           ),
-                          Text(
-                            courseTitle[i],
-                          )
-                        ],
-
-                      ),
-                      const SizedBox(width: 20,),
-                      Container(
-                        height: 30,
-                        width: 100,
-                        decoration: BoxDecoration(
-                          color: const Color(0xFF178366),
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        child: const Center(
-                          child: Text(
-                            'Start',
+                          const Text('Test',
                             style: TextStyle(
                               color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                          const Text('Chapter Knowoledge',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 13,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(22),
+                      height: 170,
+                      width: 140,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF5EBBFD),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 40,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: const Center(
+                              child: Icon(
+                                Icons.font_download_off_outlined,
+                                color: Color(0xFF5EBBFD),
+                              ),
+                            ),
+                          ),
+                          const Text('Summarize',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                          const Text('Study notes',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 13,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 30,),
+                Text('Recommended Course',
+                  style: txtThem.titleLarge,
+                ),
+                const SizedBox(height: 5,),
+                const Text('I learned Chapter 6 last time',
+                  style: txtStyle,
+                ),
+                const SizedBox(height: 20,),
+                for(var i=0; i<3; i++)
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 10),
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black12,blurRadius: 5,
+                          ),
+
+
+                        ]
+                    ),
+                    child:Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                          Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: Container(
+                              height: 40,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                color: bgColors[i],
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Center(
+                                child: courseIconsData[i],
+                              ),
+                            ),
+                          ),
+
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              courseTitle[i],
+                              style: txtThem.titleMedium,
+
+                            ),
+
+                            Text(courseText[i],
+
+                            ),
+                          ],
+
+                        ),
+                        const SizedBox(width: 20,),
+                        Container(
+                          height: 30,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            color: const Color(0xFF178366),
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              'Start',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
 
 
-            ],
+              ],
+            ),
           ),
 
 
         ),
-
+        bottomNavigationBar: const BottoomNavigatorBarScreen(),
 
       ),
     );
